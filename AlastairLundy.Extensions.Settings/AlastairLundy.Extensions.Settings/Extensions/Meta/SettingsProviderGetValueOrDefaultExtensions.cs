bfbd5b1403337b95934bf5d1799fba5cb94fdd3b
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-02-22 13:23:34 +0000

    Create SettingsProviderGetValueOrDefaultExtensions.cs

---
+++ AlastairLundy.Extensions.Settings/AlastairLundy.Extensions.Settings/Extensions/Meta/SettingsProviderGetValueOrDefaultExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using AlastairLundy.Extensions.Settings.Stores.Abstractions;
+
+namespace AlastairLundy.Extensions.Settings;
+
+public static class SettingsStoreGetValueOrDefaultExtensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="provider"></param>
+    /// <param name="key"></param>
+    /// <param name="defaultValue"></param>
+    /// <typeparam name="TValue"></typeparam>
+    /// <returns></returns>
+    public static async Task<TValue> GetValueOrDefault<TValue>(this ISettingsStore<TValue> provider, string key,
+        TValue defaultValue)
+    {
+        try
+        {
+           return await provider.GetValueAsync(key);
+        }
+        catch
+        {
+            return defaultValue;
+        }
+    }
+}
