21413aceedc3fd3f2010c849951de435eb795f11
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-02-22 14:00:34 +0000

    Update GetValueOrDefaultExtensions.cs

--- AlastairLundy.Extensions.Settings/AlastairLundy.Extensions.Configuration/Extensions/Binder/GetValueOrDefaultExtensions.cs
+++ AlastairLundy.Extensions.Settings/AlastairLundy.Extensions.Configuration/Extensions/Binder/GetValueOrDefaultExtensions.cs
@@ -1,5 +1,6 @@
 ﻿/*
-    AlastairLundy.Extensions.Configuration    Copyright (c) Alastair Lundy 2025
+    AlastairLundy.Extensions.Configuration    
+    Copyright (c) Alastair Lundy 2025
  
     This Source Code Form is subject to the terms of the Mozilla Public
     License, v. 2.0. If a copy of the MPL was not distributed with this
