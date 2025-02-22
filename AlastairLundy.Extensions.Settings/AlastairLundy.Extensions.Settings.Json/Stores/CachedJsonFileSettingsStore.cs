6923bad2df031a4fd9ca66c4d568781e99ded4ec
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-02-22 14:18:54 +0000

    start working on Json File Stores

---
+++ AlastairLundy.Extensions.Settings/AlastairLundy.Extensions.Settings.Json/Stores/CachedJsonFileSettingsStore.cs
@@ -0,0 +1,81 @@
+﻿/*
+    AlastairLundy.Extensions.Settings    
+    Copyright (c) Alastair Lundy 2025
+ 
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using AlastairLundy.Extensions.Settings.Stores.Abstractions;
+
+namespace AlastairLundy.Extensions.Settings.Json.Stores;
+
+public class CachedJsonFileSettingsStore<TValue> : JsonFileSettingsStore<TValue>, ICachedSettingsStore<TValue>
+{
+    public CachedJsonFileSettingsStore(string filePath, Func<string, TValue> toTValueConverter, Func<TValue, string> stringToStringConverter) : base(filePath, toTValueConverter, stringToStringConverter)
+    {
+        Cache = new Dictionary<string, TValue>();
+        CacheLifetime = TimeSpan.FromHours(1);
+        CacheExpiration = DateTime.Now.Add(CacheLifetime);
+    }
+
+    public Dictionary<string, TValue> Cache { get; }
+    public DateTime CacheExpiration { get; }
+    public TimeSpan CacheLifetime { get; }
+
+
+    public new async Task<TValue> GetValueAsync(string key)
+    {
+        if (Cache.Count == 0)
+        {
+            await UpdateCacheAsync();
+        }
+
+        if (Cache.ContainsKey(key) && CacheExpiration < DateTime.Now)
+        {
+            try
+            {
+                return Cache[key];
+            }
+            catch
+            {
+                return await base.GetValueAsync(key);
+            }
+        }
+        else if (CacheExpiration >= DateTime.Now)
+        {
+            await UpdateCacheAsync(CacheLifetime);
+            
+            return await base.GetValueAsync(key);
+        }
+        else
+        {
+            return await base.GetValueAsync(key);
+        }
+    }
+
+    public new Task SetValueAsync(string key, TValue value)
+    {
+        
+    }
+
+    public async Task UpdateCacheAsync()
+    {
+       await UpdateCacheAsync(CacheLifetime);
+    }
+
+    public async Task UpdateCacheAsync(TimeSpan expiry)
+    {
+        
+    }
+
+    public void ClearCache()
+    {
+        Cache.Clear();
+    }
+}
---
+++ AlastairLundy.Extensions.Settings/AlastairLundy.Extensions.Settings.Json/Stores/JsonFileSettingsStore.cs
@@ -0,0 +1,63 @@
+﻿/*
+    AlastairLundy.Extensions.Settings    
+    Copyright (c) Alastair Lundy 2025
+ 
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using AlastairLundy.Extensions.Settings.Stores.Abstractions;
+
+namespace AlastairLundy.Extensions.Settings.Json.Stores;
+
+public class JsonFileSettingsStore<TValue> : IFileSettingsStore<TValue>
+{
+    public Func<string, TValue> ToTValueConverter { get; }
+    public Func<TValue, string> ToStringConverter { get; }
+
+    public JsonFileSettingsStore(FileStoreConfiguration fileConfiguration, Func<string, TValue> toTValueConverter, Func<TValue, string> stringToStringConverter)
+    {
+        ToTValueConverter = toTValueConverter;
+        ToStringConverter = stringToStringConverter;
+        FileConfiguration = fileConfiguration;
+    }
+    
+    public async Task<TValue> GetValueAsync(string key)
+    {
+       
+    }
+
+    public async Task SetValueAsync(string key, TValue value)
+    {
+       
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public async Task<IEnumerable<string>> GetSettingsKeysAsync()
+    {
+        IEnumerable<KeyValuePair<string, TValue>> settings = await GetSettingsAsync();
+
+        return settings.Select(x => x.Key);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public async Task<IEnumerable<KeyValuePair<string, TValue>>> GetSettingsAsync()
+    {
+        
+    }
+
+    public FileStoreConfiguration FileConfiguration { get; }
+}
